e536d12b6444f8abe0befacf8fba69b0ed84bfaf
박서인 <soso176@naver.com>
2021-09-03 17:02:59 +0900

    데이터베이스 생성

--- config/production.py
+++ config/production.py
@@ -3,3 +3,9 @@ from config.default import *
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'TlKe\xbd\xe1yC\xf1\xac\xefZO\x99R\xe8'
+
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
+    user='dbmasteruser',
+    pw='qkrtjdls',
+    url='ls-00fa2416d7568d7c1eadfe3673c66ae0fe97581c.cif6jrsp3fvg.ap-northeast-2.rds.amazonaws.com',
+    db='flask_pybo')
