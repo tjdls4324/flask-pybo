f69493713d90daf8c2ffa255390bf8510526fa25
박서인 <soso176@naver.com>
2021-09-15 23:07:07 +0900

    로깅 적용

--- config/production.py
+++ config/production.py
@@ -1,3 +1,4 @@
+from logging.config import dictConfig
 from config.default import *
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
@@ -9,3 +10,26 @@ SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
     pw='qkrtjdls',
     url='ls-00fa2416d7568d7c1eadfe3673c66ae0fe97581c.cif6jrsp3fvg.ap-northeast-2.rds.amazonaws.com',
     db='flask_pybo')
+
+dictConfig({
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        }
+    },
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/myproject.log'),
+            'maxBytes': 1024 * 1024 * 5,  # 5 MB
+            'backupCount': 5,
+            'formatter': 'default',
+        },
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': ['file']
+    }
+})
